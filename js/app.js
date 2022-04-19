97a6f77c408dfa023211acb35d6f7634123593a4
faraz vaissi <farazmrx@gmail.com>
2022-04-19 21:45:04 +0430

    Add Scrolling to elements features

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
+$('a[href*="#"]')
+
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+if (
+  location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+  && 
+  location.hostname == this.hostname
+) {
+  var target = $(this.hash);
+  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+  if (target.length) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: target.offset().top
+    }, 1000, function() {
+      var $target = $(target);
+      $target.focus();
+      if ($target.is(":focus")) {
+        return false;
+      } else {
+        $target.attr('tabindex','-1');
+        $target.focus();
+      };
+    });
+  }
+}
+});
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -24,5 +54,40 @@ function showSlides(n) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  dots[slideIndex-1].className = "active";
 }
+
+$(document).ready(function () {
+    
+  $('.js--scroll-to-plans').click(function() {
+      $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+  });
+
+  $('.js--scroll-to-start').click(function() {
+      $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+  });
+
+$('.js--wp-1').waypoint(function(direction) {
+  $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+}, {
+  offset: '50%'
+});
+
+$('.js--wp-2').waypoint(function(direction) {
+  $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+}, {
+  offset: '50%'
+});
+
+$('.js--wp-3').waypoint(function(direction) {
+  $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+}, {
+  offset: '50%'
+});
+
+$('.js--wp-4').waypoint(function(direction) {
+  $('.js--wp-4').addClass('animate__animated animate__pulse');
+}, {
+  offset: '50%'
+});
+});
